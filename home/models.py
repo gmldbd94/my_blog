11284acc704443c5878713f59d2a98ce2636bc27
heeyung <gmldbd@naver.com>
2019-03-08 16:52:17 +0900

    start

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class lecture(models.Model):
+    title = models.CharField(max_length=200)
+    when = models.CharField()
+    description = models.TextField
+
+
+class study(models.Model):
+    pub_date = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    mdfile = models.FileField()
+    lecture_name = models.ForeignKey(lecture)
+
+## 수정할 내용 강의를 입력하고 매주차 강의때마다 정리할수 있는 공간을 만들자
+
